47762db5bd34b0e3235a41288efcc76fa969b12c
Ronan Amicel <ronan.amicel@gmail.com>
2011-10-20 03:21:40 +0200

    Require that a user be a sudoer.

--- fabtools/icanhaz/users.py
+++ fabtools/icanhaz/users.py
@@ -1,6 +1,7 @@
 """
 Idempotent API for managing users
 """
+from fabtools.files import is_file
 from fabtools.user import *
 
 
@@ -10,3 +11,15 @@ def user(name):
     """
     if not exists(name):
         create(name)
+
+
+def sudoer(username, hosts="ALL", operators="ALL", passwd=False, commands="ALL"):
+    """
+    I can haz sudo permissions for that user
+    """
+    tags = "PASSWD:" if passwd else "NOPASSWD:"
+    spec = "%(username)s %(hosts)s=(%(operators)s) %(tags)s %(commands)s" % locals()
+    filename = '/etc/sudoers.d/fabtools-%s' % username
+    if is_file(filename):
+        sudo('chmod 0640 %(filename)s && rm -f %(filename)s' % locals())
+    sudo('echo "%(spec)s" >%(filename)s && chmod 0440 %(filename)s' % locals(), shell=True)
