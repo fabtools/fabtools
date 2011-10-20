d5af42465166b36a662c756ec41145263b3f0635
Dominique Guardiola <contact@quinode.fr>
2011-10-20 22:00:14 +0200

    Ubuntu version parsing
    Call the right launch script for PostgreSQL based on Ubuntu version
    number

--- fabtools/icanhaz/postgres.py
+++ fabtools/icanhaz/postgres.py
@@ -5,13 +5,32 @@ from fabtools.postgres import *
 from fabtools.icanhaz.deb import package
 from fabtools.icanhaz.service import started
 
+import re
+from distutils.version import StrictVersion
+
+def check_ubuntu_version(issue):
+    result=False
+    r = re.compile("^Ubuntu ([0-9]*.[0-9]*).*$",re.IGNORECASE).findall(issue)
+    if len(r) > 0 : 
+        version_string = r[0]
+        try:
+            result = StrictVersion(version_string) > '10.04'
+        except ValueError:
+            pass
+    return(result)
+
 
 def server(version='8.4'):
     """
     I can haz PostgreSQL server
     """
-    package('postgresql-%s' % version)
-    started('postgresql-%s' % version)
+    issue = run('echo | cat /etc/issue')
+    if check_ubuntu_version(issue):
+        package('postgresql')
+        started('postgresql')
+    else:
+        package('postgresql-%s' % version)
+        started('postgresql-%s' % version)
 
 
 def user(name, password):
@@ -22,9 +41,9 @@ def user(name, password):
         create_user(name, password)
 
 
-def database(name, owner):
+def database(name, owner,postgis=False):
     """
     I can haz PostgreSQL database
     """
     if not database_exists(name):
-        create_database(name, owner)
+        create_database(name, owner, postgis)
