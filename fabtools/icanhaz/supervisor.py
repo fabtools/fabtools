f599a63786d395a9f90af735752e4f9e39e70620
Ronan Amicel <ronan.amicel@gmail.com>
2011-10-26 21:46:20 +0200

    Added package dependency for supervisor.

--- fabtools/icanhaz/supervisor.py
+++ fabtools/icanhaz/supervisor.py
@@ -1,6 +1,7 @@
 """
 Idempotent API for managing supervisor processes
 """
+from fabtools.icanhaz import deb
 from fabtools.supervisor import *
 
 
@@ -8,6 +9,7 @@ def process(name, options=None):
     """
     I can haz supervisor process
     """
+    deb.package('supervisor')
     add_process(name, options)
     reload_config()
     if process_status(name) == 'STOPPED':
