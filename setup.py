536e1c2857f9b3eb17f35510d1ff74dc7062bf1b
Ronan Amicel <ronan.amicel@gmail.com>
2012-02-19 16:56:31 +0100

    Bumped package version to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(filename):
 
 setup(
     name='fabtools',
-    version='0.1',
+    version='0.1.1',
     description='Tools for writing awesome Fabric files',
     long_description=read('README.rst'),
     author='Ronan Amicel',
