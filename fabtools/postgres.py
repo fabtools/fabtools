9cb126e0d99d9f86a8491a9cbdb315d1f31a3837
Ronan Amicel <ronan.amicel@gmail.com>
2012-06-07 04:14:23 +0200

    Refactor PostgreSQL tools

--- fabtools/postgres.py
+++ fabtools/postgres.py
@@ -6,12 +6,19 @@ from __future__ import with_statement
 from fabric.api import *
 
 
+def _run_as_pg(command):
+    """
+    Run command as 'postgres' user
+    """
+    return sudo(command, user='postgres')
+
+
 def user_exists(name):
     """
     Check if a PostgreSQL user exists
     """
     with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
-        res = sudo('''psql -t -A -c "SELECT COUNT(*) FROM pg_user WHERE usename = '%(name)s';"''' % locals(), user='postgres')
+        res = _run_as_pg('''psql -t -A -c "SELECT COUNT(*) FROM pg_user WHERE usename = '%(name)s';"''' % locals())
     return (res == "1")
 
 
@@ -19,7 +26,7 @@ def create_user(name, password):
     """
     Create a PostgreSQL user
     """
-    sudo('''psql -c "CREATE USER %(name)s WITH PASSWORD '%(password)s';"''' % locals(), user='postgres')
+    _run_as_pg('''psql -c "CREATE USER %(name)s WITH PASSWORD '%(password)s';"''' % locals())
 
 
 def database_exists(name):
@@ -27,12 +34,12 @@ def database_exists(name):
     Check if a PostgreSQL database exists
     """
     with settings(hide('running', 'stdout', 'stderr', 'warnings'), warn_only=True):
-        return sudo('''psql -d %(name)s -c ""''' % locals(), user='postgres').succeeded
+        return _run_as_pg('''psql -d %(name)s -c ""''' % locals()).succeeded
 
 
 def create_database(name, owner, template='template0', encoding='UTF8', locale='en_US.UTF-8'):
     """
     Create a PostgreSQL database
     """
-    sudo('''createdb --owner %(owner)s --template %(template)s --encoding=%(encoding)s\
- --lc-ctype=%(locale)s --lc-collate=%(locale)s %(name)s''' % locals(), user='postgres')
+    _run_as_pg('''createdb --owner %(owner)s --template %(template)s --encoding=%(encoding)s\
+ --lc-ctype=%(locale)s --lc-collate=%(locale)s %(name)s''' % locals())
