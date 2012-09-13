356f5ad09f936045892bce5552d82fca096daf2f
Ronan Amicel <ronan.amicel@gmail.com>
2012-09-14 00:40:27 +0200

    Improvements to the network module

--- fabtools/network.py
+++ fabtools/network.py
@@ -6,17 +6,39 @@ from __future__ import with_statement
 from fabric.api import *
 
 
-def address(interface):
+def interfaces():
     """
-    Get IPV4 address assigned to an interface
+    Get the list of network interfaces
+
+        >>> fabtools.network.interfaces()
+        ['eth0', 'eth1', 'lo']
+
     """
     with settings(hide('running', 'stdout')):
-        return run("/sbin/ifconfig %s | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'" % interface)
+        res = run('/sbin/ifconfig -s')
+    return map(lambda line: line.split(' ')[0], res.splitlines()[1:])
+
+
+def address(interface):
+    """
+    Get the IPv4 address assigned to an interface
+
+        >>> fabtools.network.address('lo')
+        '127.0.0.1'
+
+    """
+    with settings(hide('running', 'stdout')):
+        res = run("/sbin/ifconfig %(interface)s | grep 'inet addr'" % locals())
+    return res.split()[1].split(':')[1]
 
 
 def nameservers():
     """
     Get the list of nameserver addresses
+
+        >>> fabtools.network.nameservers()
+        ['208.67.222.222', '208.67.220.220']
+
     """
     with settings(hide('running', 'stdout')):
         res = run("cat /etc/resolv.conf | grep 'nameserver' | cut -d\  -f2")
---
+++ fabtools/tests/fabfiles/network.py
@@ -0,0 +1,52 @@
+import socket
+
+from fabric.api import task
+import fabtools
+
+
+def is_valid_ipv4_address(address):
+    try:
+        _ = socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            _ = socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:    # not a valid address
+        return False
+    return True
+
+
+def is_valid_ipv6_address(address):
+    try:
+        _ = socket.inet_pton(socket.AF_INET6, address)
+    except socket.error:    # not a valid address
+        return False
+    return True
+
+
+def is_valid_address(address):
+    return is_valid_ipv4_address(address) or is_valid_ipv6_address()
+
+
+@task
+def network():
+    """
+    Test network interfaces
+    """
+
+    interfaces = fabtools.network.interfaces()
+
+    # Check IPv4 addresses
+    for interface in interfaces:
+        ipv4_address = fabtools.network.address(interface)
+        assert is_valid_ipv4_address(ipv4_address), ipv4_address
+
+    # Check loopback interface
+    assert 'lo' in interfaces, interfaces
+    assert fabtools.network.address('lo') == '127.0.0.1'
+
+    # Check name servers
+    for address in fabtools.network.nameservers():
+        assert is_valid_address(address), address
