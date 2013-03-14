10f235f042de7be3202f0860ae7e5d223ef8d68a
Ronan Amicel <ronan.amicel@gmail.com>
2013-03-14 13:48:46 +0100

    Remove dead code
    Should have been removed by 02f587e1b95308b6

--- fabtools/require/files.py
+++ fabtools/require/files.py
@@ -135,11 +135,7 @@ def file(path=None, contents=None, source=None, url=None, md5=None,
                 (verify_remote and
                     md5sum(path, use_sudo=use_sudo) != digest.hexdigest())):
             with settings(hide('running')):
-                if source:
-                    put(source, path, use_sudo=use_sudo)
-                else:
-                    put(tmp_file.name, path, use_sudo=use_sudo)
-                    os.remove(tmp_file.name)
+                put(source, path, use_sudo=use_sudo)
 
         if t is not None:
             os.unlink(source)
