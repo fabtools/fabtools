a066bc0732d204d8d4853f6bde1616ac59d2fb97
Dominique Lederer <dominique.lederer@return1.at>
2013-03-15 10:46:21 +0100

    fix Vagrant 1.1 double quotes from ssh-config
    Vagrant 1.1 introduces leading and trailing whitespaces for IdentityFile when queried with ```vagrant ssh-config```
    this leads to
    
    ```
    IOError: [Errno 2] No such file or directory: '"/Users/xyz/.vagrant.d/insecure_private_key"'
    ```
    
    this patch removes the double quotes and keeps backward compatibility

--- fabtools/vagrant.py
+++ fabtools/vagrant.py
@@ -5,6 +5,7 @@ Vagrant helpers
 from __future__ import with_statement
 
 from fabric.api import env, hide, local, settings, task
+import re
 
 
 def ssh_config(name=''):
@@ -29,7 +30,7 @@ def _settings_dict(config):
     port = config['Port']
 
     settings['host_string'] = "%s@%s:%s" % (user, hostname, port)
-    settings['key_filename'] = config['IdentityFile']
+    settings['key_filename'] = re.sub(r'^"|"$', '', config['IdentityFile']) # strip leading and trailing double quotes, if present
     settings['forward_agent'] = (config.get('ForwardAgent', 'no') == 'yes')
     settings['disable_known_hosts'] = True
 
