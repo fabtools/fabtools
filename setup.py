f840af75813355595d253b1d05835d627d4d9cf8
Ronan Amicel <ronan.amicel@gmail.com>
2013-03-20 01:41:19 +0100

    Update required Fabric version to 1.5.2 [#94]
    We now use the `shell_env` context manager, which was introduced
    in 1.5.0, but was missing from `fabric.api` until 1.5.2.
    
    This should fix documentation build issues on Read the Docs.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     url='http://fabtools.readthedocs.org/',
     license='BSD',
     install_requires=[
-        "fabric>=1.4.0",
+        "fabric>=1.5.2",
     ],
     setup_requires=[],
     tests_require=[
