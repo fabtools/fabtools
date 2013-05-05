c901969b74073108f8d6f95b7ab125b0e3056ec3
Ronan Amicel <ronan.amicel@gmail.com>
2013-05-05 04:09:04 +0200

    Reorder functions in SSH module

--- fabtools/ssh.py
+++ fabtools/ssh.py
@@ -13,31 +13,35 @@ from fabtools.service import is_running, restart
 from fabtools.files import watch
 
 
-def _update_ssh_setting(sshd_config, name, value):
-
-    with watch(sshd_config) as config_file:
-
-        # First try to change existing setting
-        sed(sshd_config,
-            r'^(\s*#\s*)?%s\s+(yes|no)' % name,
-            '%s %s' % (name, value),
-            use_sudo=True)
-
-        # Then append setting if it's still missing
-        _append(sshd_config,
-                '%s %s' % (name, value),
-                use_sudo=True)
-
-    if config_file.changed and is_running('ssh'):
-        restart('ssh')
-
-
-def _append(filename, regex, use_sudo):
+def harden(allow_root_login=False, allow_password_auth=False,
+           sshd_config='/etc/ssh/sshd_config'):
     """
-    Less verbose append
+    Apply best practices for ssh security.
+
+    See :func:`fabtools.ssh.disable_password_auth` and
+    :func:`fabtools.ssh.disable_root_login` for a detailed
+    description.
+
+    ::
+
+        import fabtools
+
+        # This will apply all hardening techniques.
+        fabtools.ssh.harden()
+
+        # Only apply some of the techniques.
+        fabtools.ssh.harden(allow_password_auth=True)
+
+        # Override the sshd_config file location.
+        fabtools.ssh.harden(sshd_config='/etc/sshd_config')
+
     """
-    with hide('stdout', 'warnings'):
-        return append(filename, regex, use_sudo=use_sudo)
+
+    if not allow_password_auth:
+        disable_password_auth(sshd_config=sshd_config)
+
+    if not allow_root_login:
+        disable_root_login(sshd_config=sshd_config)
 
 
 def disable_password_auth(sshd_config='/etc/ssh/sshd_config'):
@@ -72,32 +76,31 @@ def enable_root_login(sshd_config='/etc/ssh/sshd_config'):
     _update_ssh_setting(sshd_config, 'PermitRootLogin', 'yes')
 
 
-def harden(allow_root_login=False, allow_password_auth=False,
-           sshd_config='/etc/ssh/sshd_config'):
+def _update_ssh_setting(sshd_config, name, value):
     """
-    Apply best practices for ssh security.
-
-    See :func:`fabtools.ssh.disable_password_auth` and
-    :func:`fabtools.ssh.disable_root_login` for a detailed
-    description.
-
-    ::
-
-        import fabtools
-
-        # This will apply all hardening techniques.
-        fabtools.ssh.harden()
-
-        # Only apply some of the techniques.
-        fabtools.ssh.harden(allow_password_auth=True)
-
-        # Override the sshd_config file location.
-        fabtools.ssh.harden(sshd_config='/etc/sshd_config')
-
+    Update a yes/no setting in the SSH config file
     """
 
-    if not allow_password_auth:
-        disable_password_auth(sshd_config=sshd_config)
+    with watch(sshd_config) as config_file:
 
-    if not allow_root_login:
-        disable_root_login(sshd_config=sshd_config)
+        # First try to change existing setting
+        sed(sshd_config,
+            r'^(\s*#\s*)?%s\s+(yes|no)' % name,
+            '%s %s' % (name, value),
+            use_sudo=True)
+
+        # Then append setting if it's still missing
+        _append(sshd_config,
+                '%s %s' % (name, value),
+                use_sudo=True)
+
+    if config_file.changed and is_running('ssh'):
+        restart('ssh')
+
+
+def _append(filename, regex, use_sudo):
+    """
+    Less verbose append
+    """
+    with hide('stdout', 'warnings'):
+        return append(filename, regex, use_sudo=use_sudo)
