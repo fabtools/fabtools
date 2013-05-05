4d97f374a5e946600aec0b1091d2be320bd464c5
Ronan Amicel <ronan.amicel@gmail.com>
2013-05-05 23:00:40 +0200

    Fix Oracle JDK unit tests

--- fabtools/tests/test_oracle_jdk.py
+++ fabtools/tests/test_oracle_jdk.py
@@ -2,26 +2,34 @@
 import mock
 import unittest
 
-from fabtools.oracle_jdk import _required_jdk_arch, _extract_jdk_version
-
 
 class OracleJdkTestCase(unittest.TestCase):
 
-    @mock.patch('fabtools.system.get_arch')
+    @mock.patch('fabtools.oracle_jdk.get_arch')
     def test_jdk_arch_for_x64_system(self, get_arch):
+
+        from fabtools.oracle_jdk import _required_jdk_arch
+
         get_arch.return_value = 'x86_64'
 
         self.assertEqual('x64', _required_jdk_arch())
 
-    @mock.patch('fabtools.system.get_arch')
+    @mock.patch('fabtools.oracle_jdk.get_arch')
     def test_jdk_arch_for_32bit_system(self, get_arch):
+
+        from fabtools.oracle_jdk import _required_jdk_arch
+
         for system_arch in ['i386', 'i486', 'i586', 'i686']:
+
             get_arch.return_value = system_arch
 
             self.assertEqual('i586', _required_jdk_arch())
 
-    @mock.patch('fabtools.system.get_arch')
+    @mock.patch('fabtools.oracle_jdk.get_arch')
     def test_jdk_arch_for_unknown_system(self, get_arch):
+
+        from fabtools.oracle_jdk import _required_jdk_arch
+
         get_arch.return_value = 'unknown'
 
         try:
@@ -31,6 +39,9 @@ class OracleJdkTestCase(unittest.TestCase):
             pass
 
     def test_jdk_version_with_update_over_ten(self):
+
+        from fabtools.oracle_jdk import _extract_jdk_version
+
         java_version_out = '''java version "1.7.0_13"
 Java(TM) SE Runtime Environment (build 1.7.0_13-b20)
 Java HotSpot(TM) Client VM (build 23.7-b01, mixed mode)
@@ -40,6 +51,9 @@ Java HotSpot(TM) Client VM (build 23.7-b01, mixed mode)
         self.assertEqual('7u13-b20', _extract_jdk_version(java_version_out))
 
     def test_jdk_version_with_update_under_ten(self):
+
+        from fabtools.oracle_jdk import _extract_jdk_version
+
         java_version_out = '''java version "1.7.0_09"
 Java(TM) SE Runtime Environment (build 1.7.0_09-b05)
 Java HotSpot(TM) 64-Bit Server VM (build 23.5-b02, mixed mode)
