6f905b29a8143c0570a06c74b8d1943e7aa2dc7e
Ronan Amicel <ronan.amicel@gmail.com>
2013-05-05 03:41:07 +0200

    Make SSH hardening more robust to config file changes

--- fabtools/ssh.py
+++ fabtools/ssh.py
@@ -6,8 +6,21 @@ This module provides tools to manage OpenSSH server and client.
 
 """
 
-from fabric.contrib.files import sed
+from fabric.api import hide
+from fabric.contrib.files import append, sed
+
 from fabtools.service import is_running, restart
+
+
+def _append(filename, regex, use_sudo):
+    """
+    Less verbose append
+    """
+    with hide('stdout', 'warnings'):
+        return append(filename, regex, use_sudo=use_sudo)
+
+
+_PASSWORD_AUTHENTICATION = r'^(\s*#\s*)?PasswordAuthentication\s+(yes|no)'
 
 
 def disable_password_auth(sshd_config='/etc/ssh/sshd_config'):
@@ -15,10 +28,16 @@ def disable_password_auth(sshd_config='/etc/ssh/sshd_config'):
     Do not allow users to use passwords to login via ssh.
     """
 
+    # First try to change existing setting
     sed(sshd_config,
-        '#PasswordAuthentication yes',
+        _PASSWORD_AUTHENTICATION,
         'PasswordAuthentication no',
         use_sudo=True)
+
+    # Then append setting if it's still missing
+    _append(sshd_config,
+            'PasswordAuthentication no',
+            use_sudo=True)
 
     if is_running('ssh'):
         restart('ssh')
@@ -29,13 +48,22 @@ def enable_password_auth(sshd_config='/etc/ssh/sshd_config'):
     Allow users to use passwords to login via ssh.
     """
 
+    # First try to change existing setting
     sed(sshd_config,
-        'PasswordAuthentication no',
-        '#PasswordAuthentication yes',
+        _PASSWORD_AUTHENTICATION,
+        'PasswordAuthentication yes',
         use_sudo=True)
+
+    # Then append setting if it's still missing
+    _append(sshd_config,
+            'PasswordAuthentication yes',
+            use_sudo=True)
 
     if is_running('ssh'):
         restart('ssh')
+
+
+_PERMIT_ROOT_LOGIN = r'^(\s*#\s*)?PermitRootLogin\s+(yes|no)'
 
 
 def disable_root_login(sshd_config='/etc/ssh/sshd_config'):
@@ -43,10 +71,16 @@ def disable_root_login(sshd_config='/etc/ssh/sshd_config'):
     Do not allow root to login via ssh.
     """
 
+    # First try to change existing setting
     sed(sshd_config,
-        'PermitRootLogin yes',
+        _PERMIT_ROOT_LOGIN,
         'PermitRootLogin no',
         use_sudo=True)
+
+    # Then append setting if it's still missing
+    _append(sshd_config,
+            'PermitRootLogin no',
+            use_sudo=True)
 
     if is_running('ssh'):
         restart('ssh')
@@ -57,10 +91,16 @@ def enable_root_login(sshd_config='/etc/ssh/sshd_config'):
     Allow root to login via ssh.
     """
 
+    # First try to change existing setting
     sed(sshd_config,
-        'PermitRootLogin no',
+        _PERMIT_ROOT_LOGIN,
         'PermitRootLogin yes',
         use_sudo=True)
+
+    # Then append setting if it's still missing
+    _append(sshd_config,
+            'PermitRootLogin yes',
+            use_sudo=True)
 
     if is_running('ssh'):
         restart('ssh')
@@ -68,7 +108,6 @@ def enable_root_login(sshd_config='/etc/ssh/sshd_config'):
 
 def harden(allow_root_login=False, allow_password_auth=False,
            sshd_config='/etc/ssh/sshd_config'):
-
     """
     Apply best practices for ssh security.
 
