f2b13109d9cf1b24f5c9323994022569c3e7b703
Andreas Kaiser <disko@binary-punks.com>
2013-06-22 13:35:54 +0200

    Adjust test to match new code.

--- fabtools/tests/test_postgres.py
+++ fabtools/tests/test_postgres.py
@@ -8,9 +8,10 @@ class PostgresTestCase(unittest.TestCase):
     @mock.patch('fabtools.require.postgres.restarted')
     @mock.patch('fabtools.require.postgres.require_locale')
     @mock.patch('fabtools.require.postgres.create_database')
+    @mock.patch('fabtools.require.postgres.run')
     @mock.patch('fabtools.require.postgres.database_exists')
-    def test_params_respected(self, database_exists, create_database,
-                              require_locale, watch, restarted, service_name):
+    def test_params_respected(self, database_exists, run, create_database,
+                              require_locale, restarted, service_name):
         """
         If require.database is called, ensure that the template,
         encoding and locale parameters are passed through to the
@@ -18,9 +19,11 @@ class PostgresTestCase(unittest.TestCase):
         """
         from fabtools import require
         database_exists.return_value = False
+        run.return_value = 'en-US.UTF-8\nde-DE.UTF-8'
         require.postgres.database('foo', 'bar', locale='some_locale',
                                   encoding='some_encoding',
                                   template='some_template')
+        run.assert_called_with('locale -a')
         require_locale.assert_called_with('some_locale')
         create_database.assert_called_with('foo', 'bar', locale='some_locale',
                                            encoding='some_encoding',
