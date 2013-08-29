9819f6d91d44f4236c292a9b0f80fa5c19d6ddfc
badele <bruno.adele@jesuislibre.org>
2013-08-29 20:45:35 +0000

    Add a function that search a HDD disk by uuid

--- fabtools/disk.py
+++ fabtools/disk.py
@@ -40,6 +40,27 @@ def partitions(device=""):
     return partitions_list
 
 
+def getdevice_by_uuid(uuid):
+    """
+    Get a HDD device by uuid
+
+    Example::
+
+        from fabtools.disk import getdevice_by_uuid 
+
+        device = getdevice_by_uuid("356fafdc-21d5-408e-a3e9-2b3f32cb2a8c")
+        if device:
+            mount(device,'/mountpoint')
+    """
+    with settings(hide('running', 'warnings', 'stdout'), warn_only=True):
+        res = run_as_root('blkid -U %s' % uuid)
+
+        if not res.succeeded:
+            return None
+
+        return res
+
+
 def mount(device, mountpoint):
     """
     Mount a partition
