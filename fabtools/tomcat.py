7d6708640e62e5f6713ce254570edb3f5c0daed1
mjbommar <michael@bommaritollc.com>
2013-11-03 09:49:03 -0500

    Applying patches mentioned in here: https://github.com/ronnix/fabtools/pull/170#issuecomment-27638007

--- fabtools/tomcat.py
+++ fabtools/tomcat.py
@@ -9,14 +9,20 @@ This module provides tools for installing `Tomcat7`_.
 """
 from __future__ import with_statement
 
+# Standard imports
+import os.path
+
+# Fabric imports
 from fabric.api import cd, hide, run, settings
 from fabtools.utils import run_as_root
 from fabtools.files import is_file, is_dir, is_link
 
-DEFAULT_VERSION = '7.0.42'
+# Default parameters
+DEFAULT_VERSION = '7.0.47'
+DEFAULT_INSTALLATION_PATH = "/usr/share/tomcat"
+DEFAULT_MIRROR="http://archive.apache.org"
 
-
-def install_from_source(version=DEFAULT_VERSION):
+def install_from_source(installation_path=DEFAULT_INSTALLATION_PATH, version=DEFAULT_VERSION, mirror=DEFAULT_MIRROR):
     """
     Install Tomcat7 from source.
 
@@ -31,29 +37,42 @@ def install_from_source(version=DEFAULT_VERSION):
     from fabtools.require import file as require_file
     from fabtools.require.files import directory as require_directory
 
-    major = version[0]
-    filename = 'apache-tomcat-%s.tar.gz' % version
-    foldername = filename[0:-7]
+    # Tokenize version into parts
+    version_tokens = version.split('.')
+    version_major = version_tokens[0]
+    version_minor = version_tokens[1]
+    version_release = version_tokens[2]
 
+    # Parse the filename and folder
+    file_name = 'apache-tomcat-{0}.tar.gz'.format(version)
+    folder_name = 'apache-tomcat-{0}'.format(version)
+
+    # Build the distribution in /tmp
     with cd('/tmp'):
-        if not is_file('/tmp/%s' % filename):
-            require_file(url='http://archive.apache.org/dist/tomcat/tomcat-%(major)s/v%(version)s/bin/%(filename)s' % {
-                'major': major,
-                'version': version,
-                'filename': filename,
-            })
-            run('tar -xzf %s' % filename)
+        if not is_file('/tmp/{0}'.format(file_name)):
+            # Ensure that the archive is in the right place
+            tomcat_url = '{3}/dist/tomcat/tomcat-{0}/v{1}/bin/{2}'.format(version_major,
+                version,
+                file_name,
+                mirror)
 
-        require_directory('/usr/share/tomcat', mode='755', use_sudo=True)
-        run_as_root('mv %s/* /usr/share/tomcat' % foldername)
-        # run('rm -rf %s' % filename)
-        # run('rm -rf %s' % foldername)
+            # Ensure the file has been downloaded
+            require_file(url=tomcat_url)
+            
+            # Extract the file
+            run('tar -xzf {0}'.format(file_name))
 
-    configure_tomcat()
+        # Ensure the directory and path match
+        require_directory(installation_path, mode='755', use_sudo=True)
+        run_as_root('mv {0}/* {1}'.format(folder_name, installation_path))
+        run("rm -f {0}".format(file_name))
+
+    # Configure and start Tomcat
+    configure_tomcat(installation_path)
     start_tomcat()
 
 
-def configure_tomcat():
+def configure_tomcat(installation_path):
     from fabric.contrib.files import append, sed
 
     startupScript = """
@@ -65,17 +84,17 @@ def configure_tomcat():
 
 case $1 in
 start)
-sh /usr/share/tomcat/bin/startup.sh
+sh {0}/bin/startup.sh
 ;;
 stop)
-sh /usr/share/tomcat/bin/shutdown.sh
+sh {0}/bin/shutdown.sh
 ;;
 restart)
-sh /usr/share/tomcat/bin/shutdown.sh
-sh /usr/share/tomcat/bin/startup.sh
+sh {0}/bin/shutdown.sh
+sh {0}/bin/startup.sh
 ;;
 esac
-exit 0"""
+exit 0""".format(installation_path)
 
     if not is_file('/etc/init.d/tomcat'):
         append('/etc/init.d/tomcat', startupScript, use_sudo=True)
@@ -96,14 +115,14 @@ def stop_tomcat():
     run_as_root('service tomcat stop')
 
 
-def version():
+def version(installation_path):
     """
     Get the version of currently installed tomcat.
 
     Returns ``None`` if it is not installed.
     """
     with settings(hide('running', 'stdout', 'warnings'), warn_only=True):
-        res = run('/usr/share/tomcat/bin/version.sh')
+        res = run('{0}/bin/version.sh'.format(installation_path))
     if res.failed:
         return None
     else:
