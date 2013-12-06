9c1583f0fb9ef1aa2a52a2604aa2a4573cd91fd0
Ronan Amicel <ronan.amicel@gmail.com>
2013-12-06 21:56:28 +0100

    Use the new Read the Docs theme if available
    When building docs locally, we can also use the beautiful new theme
    introduced by Read the Docs:
    
        $ pip install -r dev-requirements.txt
        $ cd docs/
        $ make html
        $ open _build/html/index.html

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
+# Use the new Read the Docs theme if available
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# Fall back to the default theme
+except ImportError:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
