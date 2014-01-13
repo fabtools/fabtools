76791214b52edf19ff1543bc819184b2e82918c9
Ronan Amicel <ronan.amicel@gmail.com>
2014-01-13 21:22:00 +0100

    Refactor APT key functional test

--- fabtools/tests/fabfiles/deb.py
+++ fabtools/tests/fabfiles/deb.py
@@ -1,81 +1,108 @@
 from __future__ import with_statement
 
 from fabric.api import (
+    hide,
     run,
+    settings,
     task,
 )
+from fabtools.utils import run_as_root
 
 
 def reset():
-    from fabric.api import settings
-    from fabtools.utils import run_as_root
-
-    with settings(warn_only=True):
+    with settings(hide('output', 'warnings'), warn_only=True):
         run_as_root('apt-key del 7BD9BF62')
         run_as_root('apt-key del C4DEFFEB')
 
 
 @task
-def deb():
-    """
-    Check deb functions.
-    """
-
-    from fabtools import require
-    from fabtools import deb
-    from fabtools.utils import run_as_root
-
-    # deb.add_apt_key with keyid
+def test_add_apt_key_with_key_id_from_url():
+    from fabtools.deb import add_apt_key
     reset()
-    deb.add_apt_key(keyid='C4DEFFEB', url='http://repo.varnish-cache.org/debian/GPG-key.txt')
+    add_apt_key(keyid='C4DEFFEB', url='http://repo.varnish-cache.org/debian/GPG-key.txt')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
 
+
+@task
+def test_add_apt_key_with_key_id_from_default_key_server():
+    from fabtools.deb import add_apt_key
     reset()
-    deb.add_apt_key(keyid='7BD9BF62')
-    deb.add_apt_key(keyid='7BD9BF62') # Intentionally repeated
+    add_apt_key(keyid='7BD9BF62')
+    add_apt_key(keyid='7BD9BF62') # Intentionally repeated
     run_as_root('apt-key finger | grep -q 7BD9BF62')
 
+
+@task
+def test_add_apt_key_with_key_id_from_specific_key_server():
+    from fabtools.deb import add_apt_key
     reset()
-    deb.add_apt_key(keyid='7BD9BF62', keyserver='keyserver.ubuntu.com')
+    add_apt_key(keyid='7BD9BF62', keyserver='keyserver.ubuntu.com')
     run_as_root('apt-key finger | grep -q 7BD9BF62')
 
+
+@task
+def test_add_apt_key_with_key_id_from_file():
+    from fabtools.deb import add_apt_key
     reset()
     run('wget http://repo.varnish-cache.org/debian/GPG-key.txt -O /tmp/tmp.fabtools.test.key')
-    deb.add_apt_key(keyid='C4DEFFEB', filename='/tmp/tmp.fabtools.test.key')
+    add_apt_key(keyid='C4DEFFEB', filename='/tmp/tmp.fabtools.test.key')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
 
 
-    # deb.add_apt_key without keyid
+@task
+def test_add_apt_key_without_key_id_from_url():
+    from fabtools.deb import add_apt_key
     reset()
-    deb.add_apt_key(url='http://repo.varnish-cache.org/debian/GPG-key.txt')
+    add_apt_key(url='http://repo.varnish-cache.org/debian/GPG-key.txt')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
 
+
+@task
+def test_add_apt_key_without_key_id_from_default_key_server():
+    from fabtools.deb import add_apt_key
     reset()
-    deb.add_apt_key(keyid='7BD9BF62')
+    add_apt_key(keyid='7BD9BF62')
     run_as_root('apt-key finger | grep -q 7BD9BF62')
 
+
+@task
+def test_add_apt_key_without_key_id_from_file():
+    from fabtools.deb import add_apt_key
     reset()
     run('wget http://repo.varnish-cache.org/debian/GPG-key.txt -O /tmp/tmp.fabtools.test.key')
-    deb.add_apt_key(filename='/tmp/tmp.fabtools.test.key')
+    add_apt_key(filename='/tmp/tmp.fabtools.test.key')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
 
-    # require.deb.key
+
+@task
+def test_require_deb_key_from_url():
+    from fabtools.require.deb import key as require_key
     reset()
-    require.deb.key(keyid='C4DEFFEB', url='http://repo.varnish-cache.org/debian/GPG-key.txt')
+    require_key(keyid='C4DEFFEB', url='http://repo.varnish-cache.org/debian/GPG-key.txt')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
 
+
+@task
+def test_require_deb_key_from_default_key_server():
+    from fabtools.require.deb import key as require_key
     reset()
-    require.deb.key(keyid='7BD9BF62')
-    require.deb.key(keyid='7BD9BF62') # Intentionally repeated
+    require_key(keyid='7BD9BF62')
+    require_key(keyid='7BD9BF62') # Intentionally repeated
     run_as_root('apt-key finger | grep -q 7BD9BF62')
 
+
+@task
+def test_require_deb_key_from_specific_keyserver():
+    from fabtools.require.deb import key as require_key
     reset()
-    require.deb.key(keyid='7BD9BF62', keyserver='keyserver.ubuntu.com')
+    require_key(keyid='7BD9BF62', keyserver='keyserver.ubuntu.com')
     run_as_root('apt-key finger | grep -q 7BD9BF62')
 
+
+@task
+def test_require_deb_key_from_file():
+    from fabtools.require.deb import key as require_key
     reset()
     run('wget http://repo.varnish-cache.org/debian/GPG-key.txt -O /tmp/tmp.fabtools.test.key')
-    require.deb.key(keyid='C4DEFFEB', filename='/tmp/tmp.fabtools.test.key')
+    require_key(keyid='C4DEFFEB', filename='/tmp/tmp.fabtools.test.key')
     run_as_root('apt-key finger | grep -q C4DEFFEB')
-
-
