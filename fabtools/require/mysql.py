f7e851efaab057becce4ce0b8bd5fba146bf34c3
Ronan Amicel <ronan.amicel@gmail.com>
2015-06-18 22:00:20 +0200

    Add RedHat support for MySQL [#197]

--- fabtools/require/mysql.py
+++ fabtools/require/mysql.py
@@ -7,15 +7,19 @@ and creating MySQL users and databases.
 
 """
 
-from fabric.api import hide, prompt, settings
+from pipes import quote
+
+from fabric.api import hide, prompt, run, settings
 
 from fabtools.mysql import (
     create_database,
     create_user,
     database_exists,
+    query,
     user_exists,
 )
 from fabtools.system import UnsupportedFamily, distrib_family
+from fabtools.utils import run_as_root
 
 from fabtools.require.service import started
 
@@ -34,8 +38,10 @@ def server(version=None, password=None):
     family = distrib_family()
     if family == 'debian':
         _server_debian(version, password)
+    elif family == 'redhat':
+        _server_redhat(version, password)
     else:
-        raise UnsupportedFamily(supported=['debian'])
+        raise UnsupportedFamily(supported=['debian', 'redhat'])
 
 
 def _server_debian(version, password):
@@ -61,6 +67,33 @@ def _server_debian(version, password):
         require_deb_package(pkg_name)
 
     started('mysql')
+
+
+def _server_redhat(version, password):
+
+    from fabtools.require.rpm import package as require_rpm_package
+
+    require_rpm_package('mysql-server')
+    run_as_root('chkconfig --levels 235 mysqld on')
+    run_as_root('service mysqld start')
+    _require_root_password(password)
+
+
+def _require_root_password(password):
+    quoted_password = quote(password)
+    if not _is_root_password_set(quoted_password):
+        _set_root_password(quoted_password)
+
+
+def _is_root_password_set(quoted_password):
+    cmd = 'mysql --user=root --password={password} --execute="select 1;"'.format(password=quoted_password)
+    res = run(cmd, quiet=True)
+    return res.succeeded
+
+
+def _set_root_password(quoted_password):
+    run('/usr/bin/mysqladmin --user=root password {password}'.format(password=quoted_password))
+    run('/usr/bin/mysqladmin --user=root --password={password} -h localhost.localdomain password {password}'.format(password=quoted_password))
 
 
 def user(name, password, **kwargs):
