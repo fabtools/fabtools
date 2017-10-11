736818de051284448b61e4bd9eaee16c227c13e2
Renato Santos <renato.santos@wplex.com.br>
2017-10-11 14:36:56 -0300

    Add command ip

--- fabtools/network.py
+++ fabtools/network.py
@@ -16,12 +16,17 @@ def interfaces():
         if is_file('/usr/sbin/dladm'):
             res = run('/usr/sbin/dladm show-link')
             result = map(lambda line: line.split(' ')[0], res.splitlines()[1:])
-        else is_file('/sbin/ifconfig'):
+        elif is_file('/sbin/ifconfig'):
             res = sudo('/sbin/ifconfig -s')
             result = map(lambda line: line.split(' ')[0], res.splitlines()[1:])
             result.remove('Iface')
+        elif is_file('/sbin/ip'):
+            res = run('/sbin/ip l')
+            res = map(lambda line: line.split(' ')[1], res.splitlines()[1:])
+            result = map(lambda line: line.split('@')[0], res)
+        else:
+            return "Dont have command [dladm|ifconfig|ip]"
     return result
-
 
 def address(interface):
     """
@@ -39,9 +44,9 @@ def address(interface):
     with settings(hide('running', 'stdout')):
         if is_file('/sbin/ifconfig'):
             res = sudo("/sbin/ifconfig %(interface)s | grep 'inet '" % locals())
-        elif is_file('/sbin/ip'):
+        else:
             res = run("/sbin/ip a show %(interface)s | grep 'inet '" % locals())
-    if interface != "lo":
+    if interface != "lo" and interface != "":
         if 'addr' in res:
             if 'sudo' in res:
                 res = map(lambda line: line.split('addr:')[1], res.splitlines()[1:])
@@ -51,41 +56,6 @@ def address(interface):
                 return res.split()[1].split(':')[1]
         else:
             return res.split()[1]
-
-def ipv6_addresses(interface):
-    """
-    Get the IPv6 addresses assigned to an interface. Returns a dictionary of
-    {scope: ipv6_address} pairs.
-
-    Example::
-
-        import fabtools
-
-        # Print all triples of interface, scope, IP addresses
-        for interface in fabtools.network.interfaces():
-            for scope, addr in fabtools.network.ipv6_addresses(interface).items():
-                print("{} {} {}".format(interface, scope, addr))
-
-    """
-    with settings(hide('running', 'stdout')):
-        res = sudo("/sbin/ifconfig %(interface)s | grep 'inet6 ' || true" % locals())
-    ret = {}
-    if res == "":
-        return ret
-    lines = res.split("\n")
-    for line in lines:
-        if 'addr' in line:
-            addr = res.split()[2]
-        else:
-            addr = res.split()[1]
-        lower_line = line.lower()
-        addr_scope = 'unknown'
-        for scope in ['host', 'link', 'global']:
-            if scope in lower_line:
-                addr_scope = scope
-        ret[addr_scope] = addr
-    return ret
-
 
 def mac(interface):
     """
